aff379f671209685f19f23ff4130a7750a120250
Rachael Ewins <rachaelewins@hotmail.com>
2021-06-23 07:23:01 +0200

    basic functionallity

--- src/index.js
+++ src/index.js
@@ -21,7 +21,5 @@ app.on("ready", () => {
 });
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  app.quit();
 });
--- src/simulation.js
+++ src/simulation.js
@@ -1,26 +1,57 @@
-const simultionBox = document.getElementById('simulation-box')
+const simulationBox = document.getElementById('simulation-box')
 
-var boidList = [
-  {position_x: 50, position_y: 50, angle:0},
-  {position_x: 100, position_y: 50, angle:45},
-  {position_x: 150, position_y: 50, angle:90},
-  {position_x: 200, position_y: 50, angle:100}
-]
-drawSimulation(simultionBox, boidList)
+const SPEED = 2
+var boidList = []
 
-function drawSimulation(simulationBox, boidList) {
-  boidList.forEach(boid => {
-    displayOneBoid(simulationBox, boid)
-  });
+createDisplay(simulationBox, boidList)
+
+function createDisplay(simulationBox, boidList) {
+  for(var i = 0; i < 20; i++) {
+    boidList.push({x: Math.random()*800, y: Math.random()*600, angle: Math.random()*180})
+    console.log(boidList[i], "boid")
+    const boidDisplay = document.createElement('div')
+    boidDisplay.setAttribute("id", `${i}`)
+    boidDisplay.style.left = `${boidList[i].x}px`
+    boidDisplay.style.top = `${boidList[i].y}px`
+    boidDisplay.style.transform = `translate(-50%, -50%) rotate(${boidList[i].angle}deg)`
+    boidDisplay.classList.add("boid")
+    console.log(boidDisplay, "boidDisplay")
+    simulationBox.appendChild(boidDisplay)
+  }
 }
 
-function displayOneBoid(simulationBox, boid) {
-  console.log(boid, "boid")
-  const boidDisplay = document.createElement('div')
-  boidDisplay.style.left = `${boid.position_x}px`
-  boidDisplay.style.top = `${boid.position_y}px`
+setInterval(() => {
+  drawSimulation(boidList)
+}, 50);
+
+function drawSimulation(boidList) {
+  for(var i = 0; i< boidList.length; i++) {
+    stepForward(boidList[i], boidList)
+    displayOneBoid(boidList[i], i)
+  }
+}
+
+function stepForward(boid, boidList) {
+  boidList.forEach(otherBoid => {
+    if(otherBoid === boid) {
+      return true
+    }
+    var distance = ((otherBoid.x - boid.x)**2 + (otherBoid.y - boid.y)**2)**0.5
+    if( distance < 50 ) {
+      boid.angle += Math.sin((boid.angle - otherBoid.angle + 180) * Math.PI / 180) * 10/(distance)**0.5
+      boid.angle = (boid.angle+360)%360
+    }
+  });
+  boid.x += SPEED * Math.sin(boid.angle * Math.PI / 180)
+  boid.y += SPEED * -Math.cos(boid.angle * Math.PI / 180)
+  boid.x = (boid.x+800)%800
+  boid.y = (boid.y+600)%600
+}
+
+function displayOneBoid(boid, index) {
+  const boidDisplay = document.getElementById(`${index}`)
+  boidDisplay.style.left = `${boid.x}px`
+  boidDisplay.style.top = `${boid.y}px`
   boidDisplay.style.transform = `translate(-50%, -50%) rotate(${boid.angle}deg)`
   boidDisplay.classList.add("boid")
-  console.log(boidDisplay, "boidDisplay")
-  simulationBox.appendChild(boidDisplay)
 }
