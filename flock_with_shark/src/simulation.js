fe6cf29e74caed6eddffb563ffe731f7a75e6871
Rachael Ewins <rachaelewins@hotmail.com>
2021-06-27 10:25:43 +0200

    better html layout

--- flock_with_shark/src/simulation.js
+++ flock_with_shark/src/simulation.js
@@ -2,69 +2,73 @@ const { createFish } = require("./src/fish")
 const { createSharks } = require("./src/shark")
 
 const startingFormButton = document.querySelector("#starting-form")
-const resetFormButton = document.querySelector("#reset-form")
+const resetButton = document.querySelector("#reset")
 let runningSim = false
+let fishList = []
+let sharkList = []
+let simulation
 
-if (!runningSim) {
-  startingFormButton.addEventListener("submit", function(event){
-    event.preventDefault();   // stop the form from submitting
-    let fishNumber = document.getElementById("fish-number").value
-    let sharkNumber = document.getElementById("shark-number").value
-    
-    var fishList = createFish(fishNumber)
-    var sharkList = createSharks(sharkNumber)
 
-    const simulationBox = document.getElementById('simulation-box')
-    simulationBox.style.width = "1200px"
-    simulationBox.style.height = "600px"
-    simulationBox.style.visibility="visible"
-    startingFormButton.style.visibility="hidden"
-    resetFormButton.style.visibility="visible"
+startingFormButton.addEventListener("submit", function(event){
+  event.preventDefault();   // stop the form from submitting
+  let fishNumber = document.getElementById("fish-number").value
+  let sharkNumber = document.getElementById("shark-number").value
+  
+  fishList = createFish(fishNumber)
+  sharkList = createSharks(sharkNumber)
 
-    createDisplay(simulationBox, fishList, sharkList)
+  const simulationBox = document.getElementById('simulation-box')
+  simulationBox.style.height = "600px"
+  simulationBox.style.visibility = "visible"
+  startingFormButton.style.visibility = "hidden"
+  resetButton.style.visibility = "visible"
 
-    runningSim = true
-    let simulation = setInterval(() => {
-      drawSimulation(fishList, sharkList)
-    }, 50);
+  createDisplay(simulationBox, fishList, sharkList)
 
-  })
-} else {
-  resetFormButton.addEventListener("submit", function(event){
-    event.preventDefault();   // stop the form from submitting
+  runningSim = true 
+  simulation = setInterval(() => {
+    drawSimulation(fishList, sharkList)
+  }, 50);
+})
 
-    const simulationBox = document.getElementById('simulation-box')
-    simulationBox.style.width = "1200px"
-    simulationBox.style.height = "0px"
-    simulationBox.style.visibility="hidden"
-    startingFormButton.style.visibility="visible"
-    resetFormButton.style.visibility="hidden"
 
-    runningSim = false
-    clearInterval(simulation)
-  })
+resetButton.addEventListener("click", function(event){
+  event.preventDefault();   // stop the form from submitting
 
-}
+  const simulationBox = document.getElementById('simulation-box')
+  // simulationBox.innerHTML=""
+  simulationBox.style.height = "0px"
+  simulationBox.style.visibility = "hidden"
+  startingFormButton.style.visibility = "visible"
+  resetButton.style.visibility = "hidden"
+
+  runningSim = false
+  clearInterval(simulation)
+})
 
 function createDisplay(simulationBox, fishList, sharkList) {
   fishList.forEach(fish => {
-    const fishDisplay = document.createElement('div')
-    fishDisplay.setAttribute("id", `fish-${fish.id}`)
-    fishDisplay.style.left = `${fish.x +25}px`
-    fishDisplay.style.top = `${fish.y +25}px`
-    fishDisplay.style.transform = `translate(-50%, -50%) rotate(${fish.angle}rad)`
-    fishDisplay.classList.add("fish")
-    simulationBox.appendChild(fishDisplay)
+    if (!document.getElementById(`fish-${fish.id}`)) {
+      const fishDisplay = document.createElement('div')
+      fishDisplay.setAttribute("id", `fish-${fish.id}`)
+      fishDisplay.style.left = `${fish.x +25}px`
+      fishDisplay.style.top = `${fish.y +25}px`
+      fishDisplay.style.transform = `translate(-50%, -50%) rotate(${fish.angle}rad)`
+      fishDisplay.classList.add("fish")
+      simulationBox.appendChild(fishDisplay)
+    }
   })
   sharkList.forEach(shark => {
-    const sharkDisplay = document.createElement('div')
-    sharkDisplay.setAttribute("id", `shark-${shark.id}`)
-    sharkDisplay.style.left = `${shark.x +25}px`
-    sharkDisplay.style.top = `${shark.y +25}px`
-    sharkDisplay.style.borderWidth = `0 7px 20px 7px`
-    sharkDisplay.style.transform = `translate(-50%, -50%) rotate(${shark.angle}rad)`
-    sharkDisplay.classList.add("shark")
-    simulationBox.appendChild(sharkDisplay)
+    if (!document.getElementById(`shark-${shark.id}`)) {
+      const sharkDisplay = document.createElement('div')
+      sharkDisplay.setAttribute("id", `shark-${shark.id}`)
+      sharkDisplay.style.left = `${shark.x +25}px`
+      sharkDisplay.style.top = `${shark.y +25}px`
+      sharkDisplay.style.borderWidth = `0 7px 20px 7px`
+      sharkDisplay.style.transform = `translate(-50%, -50%) rotate(${shark.angle}rad)`
+      sharkDisplay.classList.add("shark")
+      simulationBox.appendChild(sharkDisplay)
+    }
   })
 }
 
